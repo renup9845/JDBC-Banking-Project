916943c685e8a6734093515e4fd014c18e578c4f
renup9845 <110659991+renup9845@users.noreply.github.com>
2022-09-08 18:23:00 +0530

    Update bankingdao.java

--- bankingdao.java
+++ bankingdao.java
@@ -1,3 +1,9 @@
+/**
+ * Illustrating BANK MANAGEMENT SYSTEM TO CREATE BANK ACCOUNT ADD ACCOUNT DETAILS TO DATABASE AND PRINT DATA OF ONE OR ALL ACCOUNT USING DATA ACCESS OBJECT
+ * PERFORMING MONEY DEPOSIT AND MONEY WITHDRAWING FROM ACCOUNT BALANCE
+ * By Renu
+ */
+
 package com.slot2jdbc;
 import java.sql.*;
 public class bankingdao {
@@ -8,7 +14,7 @@ public class bankingdao {
 		
 		//Getting Connection with DataBase
 		Class.forName("com.mysql.cj.jdbc.Driver");
-	con = DriverManager.getConnection("jdbc:mysql://localhost:3306/anudipdeeptech","root","96430");
+	        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/anudipdeeptech","root","96430");
 	
 	}
 	
@@ -157,8 +163,6 @@ public class bankingdao {
 
 	
 }
-
-
 }
 
 
